ae511edbf64fc73e22a7c7b0f16454b7ebc16d69
JMaity981 <jayantamaity981@gmail.com>
2024-02-14 01:21:56 +0530

    14-02-2024-01-20-Import-Export-Module
    Create and Export our own Module

---
+++ ImportExport/index.js
@@ -0,0 +1,8 @@
+// const add = require("./operation");
+const  {add,sub,multiplication,devide,name} = require("./operation");
+
+console.log("4 + 6 = " + add(4,6));
+console.log("15 - 6 = " + sub(15,6));
+console.log("4 * 6 = " + multiplication(4,6));
+console.log("20 / 3 = " + devide(20,3));
+console.log("name = " + name);
---
+++ ImportExport/operation.js
@@ -0,0 +1,18 @@
+const add = (a,b) => {
+    return a+b;
+}
+
+const sub = (a,b) => {
+    return a-b;
+}
+
+const multiplication = (a,b) => {
+    return a*b;
+}
+
+const devide = (a,b) => {
+    return a/b;
+}
+const name = "JAYANTA";
+// module.exports = add;
+module.exports = {add,sub,multiplication,devide,name};
