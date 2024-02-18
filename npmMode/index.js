2ccee675079a545a72d4903302a48f4244848e18
JMaity981 <jayantamaity981@gmail.com>
2024-02-18 21:57:42 +0530

    chalk example
    https://www.npmjs.com/package/chalk

--- npmMode/index.js
+++ npmMode/index.js
@@ -1,3 +1,9 @@
 const chalk = require("chalk");
 
 console.log(chalk.blue("Hello world"));
+
+console.log(chalk.blue.underline.inverse("Hello world"));
+
+console.log(chalk.green.inverse("TRUE"));
+
+console.log(chalk.red.inverse("FALSE"));
