4e23af0640535ef331a473bb69b47bbda509f519
JMaity981 <jayantamaity981@gmail.com>
2024-02-18 22:23:49 +0530

    valdator ex.
    https://www.npmjs.com/package/validator

--- npmMode/index.js
+++ npmMode/index.js
@@ -4,6 +4,13 @@ console.log(chalk.blue("Hello world"));
 
 console.log(chalk.blue.underline.inverse("Hello world"));
 
-console.log(chalk.green.inverse("TRUE"));
+console.log(chalk.green.inverse("Success"));
 
-console.log(chalk.red.inverse("FALSE"));
+console.log(chalk.red.inverse("Error"));
+
+
+const validator = require("validator");
+const res = validator.isEmail('jayanta@cloudoter.com'); //=> true
+const res2 = validator.isEmail('jayanta@com'); //=> false
+console.log(res ? chalk.green.inverse(res):chalk.red.inverse(res));
+console.log(res2 ? chalk.green.inverse(res2):chalk.red.inverse(res2));
