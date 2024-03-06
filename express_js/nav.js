656e498ade3fd95f62ca9c17d72cbb5abd551059
JMaity981 <jayantamaity981@gmail.com>
2024-03-06 22:09:34 +0530

    #28 Send Html & Json Data as Response

--- express_js/nav.js
+++ express_js/nav.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to My Home Page");
+    res.write("<h1>Welcome to My Home Page<h1>");
+    res.write("<h1>Welcome Page<h1>");
+    res.send();
 });
 
 app.get('/about',(req,res)=>{
@@ -14,6 +16,22 @@ app.get('/contact',(req,res)=>{
     res.send("Welcome to My Contact Page");
 });
 
+app.get('/temp',(req,res)=>{
+    res.json([
+        {
+            temp : 14,
+            type : "Cloud"
+        },
+        {
+            temp : 20,
+            type : "Sunny"
+        },
+        {
+            temp : 12,
+            type : "Rainy"
+        },
+    ]);
+});
 
 app.listen(port,()=>{
     console.log(`Listening to Port No. ${port}`);
