c8bd85640165574b8f4b67e7ad1ecbd89c075b12
JMaity981 <jayantamaity981@gmail.com>
2024-03-31 03:11:06 +0530

    #51 Create and Insert the Document
    Create and Insert the Document using Express in MongoDB using Mongoose

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -22,3 +22,23 @@ const playlistScheema = new mongoose.Schema({
 
 //collection creation
 const Playlist = new mongoose.model("playlist",playlistScheema);
+
+//create document or insert
+const createCocument = async() =>{
+    try{
+        const nodePlaylist = new Playlist({
+            name: "React JS",
+            ctype: "Front End",
+            videos: 40,
+            author: "J. Maity",
+            active: true
+        });
+        
+        const result = await nodePlaylist.save();
+        console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+createCocument();
