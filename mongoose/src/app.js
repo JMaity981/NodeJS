29d5ff07b819d23ee1812e46a13f1061cdbb8572
JMaity981 <jayantamaity981@gmail.com>
2024-03-31 02:52:00 +0530

    # 52 Create Collections using Mongoose
    Mongoose Schema and Models

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -4,3 +4,21 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/technical")
 .then( () => console.log("Connection Successfull..."))
 .catch( (err) => console.log(err) );
+
+const playlistScheema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type:Date,
+        default: Date.now
+    }
+});
+
+//collection creation
+const Playlist = new mongoose.model("playlist",playlistScheema);
